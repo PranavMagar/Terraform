7059d64c5d56594ee08062590de0945b86a33be7
Pranav <pranav.magar5001@gmail.com>
2025-07-09 23:57:57 +0530

    commiting

--- AWS-EC2-Provisioning/backend.tf
+++ AWS-EC2-Provisioning/backend.tf
@@ -1,4 +1,4 @@
-terraform {
+/*terraform {
   backend "s3" {
     bucket         = "bucketforterraformprojects"
     key            = "terraform/state/terraform.tfstate"
@@ -8,7 +8,7 @@ terraform {
   }
 }
 
-// resource "aws_s3_bucket" "s3_bucket"{
-//      bucket = "Pranav's-s3-bucket"
- //   }
+ resource "aws_s3_bucket" "s3_bucket"{
+      bucket = "Pranav's-s3-bucket"
+   }
 
